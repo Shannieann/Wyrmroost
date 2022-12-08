e624ed90b28c94767ac38230cbfb784e473ec504
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-08 15:53:45 -0500

    Extract Navigation Logic to super class

--- src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ButterflyLeviathanEntity.java
+++ src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ButterflyLeviathanEntity.java
@@ -91,7 +91,7 @@ public class ButterflyLeviathanEntity extends WRDragonEntity
     {
         super(entityType, level);
         noCulling = WRConfig.NO_CULLING.get();
-        moveControl = new MoveController();
+        //moveControl = new MoveController();
         maxUpStep = 2;
         setPathfindingMalus(BlockPathTypes.WATER, 0);
     }
@@ -624,12 +624,13 @@ public class ButterflyLeviathanEntity extends WRDragonEntity
         return true;
     }
 
-
+    /*
     @Override
     protected PathNavigation createNavigation(Level level)
     {
         return new Navigator();
     }
+    */
 
 
     @Override
@@ -708,78 +709,6 @@ public class ButterflyLeviathanEntity extends WRDragonEntity
             else if (reason == MobSpawnType.NATURAL) return chance < 0.001;
         }
         return false;
-    }
-
-
-    public class Navigator extends WaterBoundPathNavigation
-    {
-        public Navigator()
-        {
-            super(ButterflyLeviathanEntity.this, ButterflyLeviathanEntity.this.level);
-        }
-
-        @Override
-        protected PathFinder createPathFinder(int range)
-        {
-            return new PathFinder(nodeEvaluator = new SwimNodeEvaluator(true), range);
-        }
-
-        @Override
-        public boolean isStableDestination(BlockPos pos)
-        {
-            return !level.getBlockState(pos.below()).isAir();
-        }
-
-        @Override
-        protected boolean canUpdatePath()
-        {
-            return true;
-        }
-    }
-
-    private class MoveController extends MoveControl
-    {
-        public MoveController()
-        {
-            super(ButterflyLeviathanEntity.this);
-        }
-
-        public void tick()
-        {
-            if (operation == Operation.MOVE_TO && !canBeControlledByRider())
-            {
-                operation = Operation.WAIT;
-                double x = wantedX - getX();
-                double y = wantedY - getY();
-                double z = wantedZ - getZ();
-                double distSq = x * x + y * y + z * z;
-                if (distSq < 2.5000003E-7) setSpeed(0f); // why move...
-                else
-                {
-                    float newYaw = (float) Math.toDegrees(Mth.atan2(z, x)) - 90f;
-                    float pitch = -((float) (Mth.atan2(y, Mth.sqrt((float)(x * x + z * z))) * 180 / Math.PI));
-                    pitch = Mth.clamp(Mth.wrapDegrees(pitch), -85f, 85f);
-
-                    yHeadRot = newYaw;
-                    yBodyRot = yRot = rotlerp(yRot, yHeadRot, getYawRotationSpeed());
-                    pitch = rotlerp(pitch, pitch, 75);
-                    ((LessShitLookController) getLookControl()).stopLooking();
-                    float speed = isInWater()? (float) getAttributeValue(ForgeMod.SWIM_SPEED.get()) : (float) getAttributeValue(MOVEMENT_SPEED);
-                    setSpeed(speed);
-                    if (isInWater())
-                    {
-                        zza = Mth.cos(pitch * (Mafs.PI / 180f)) * speed;
-                        yya = -Mth.sin(pitch * (Mafs.PI / 180f)) * speed;
-                    }
-                }
-            }
-            else
-            {
-                setSpeed(0);
-                setZza(0);
-                setYya(0);
-            }
-        }
     }
 
     private class AttackGoal extends Goal
