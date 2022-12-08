6a68c4909d1cde345f4e53cbc2b7d4305b2e9090
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-08 15:51:05 -0500

    Refacotring

--- src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/goals/AnimatedGoal.java
+++ src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/goals/AnimatedGoal.java
@@ -1,4 +1,4 @@
-package com.github.shannieann.wyrmroost.entities.dragon.ai;
+package com.github.shannieann.wyrmroost.entities.dragon.ai.goals;
 
 import com.github.shannieann.wyrmroost.entities.dragon.WRDragonEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
