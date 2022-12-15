58e0b7dbc4230e5247f47b80251ee8d0b865abd3
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-15 11:15:30 -0500

    BFL head yaw

--- src/main/java/com/github/shannieann/wyrmroost/client/model/entity/dragon/ModelButterflyLeviathan.java
+++ src/main/java/com/github/shannieann/wyrmroost/client/model/entity/dragon/ModelButterflyLeviathan.java
@@ -3,8 +3,11 @@ package com.github.shannieann.wyrmroost.client.model.entity.dragon;
 import com.github.shannieann.wyrmroost.Wyrmroost;
 import com.github.shannieann.wyrmroost.entities.dragon.EntityButterflyLeviathan;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class ModelButterflyLeviathan extends AnimatedGeoModel<EntityButterflyLeviathan>
 {
@@ -34,16 +37,14 @@ public class ModelButterflyLeviathan extends AnimatedGeoModel<EntityButterflyLev
         float rotationYawMultiplier = 1.4F;
         float rotationPitchMultiplier = 10.0F;
 
-        //TODO: Head pitch conflicts with dynamic pitch, enable head yaw only
-        //TODO: Test head pitch again. Ensure maxRot is not excessive.
-        /*
+
         IBone head = this.getAnimationProcessor().getBone("head");
         EntityModelData extraData = (EntityModelData) animationEvent.getExtraDataOfType(EntityModelData.class).get(0);
         if (head != null) {
-            head.setRotationX(extraData.headPitch * Mth.DEG_TO_RAD);
             head.setRotationY(extraData.netHeadYaw * Mth.DEG_TO_RAD);
         }
-        */
+        //TODO: Head Pitch... Ony if on ground
+
 
         float setPitchValue;
         if (animatable.isUsingSwimmingNavigator()) {
