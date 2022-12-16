737c5046e95a12102db9eaa3c81d125d3db05eee
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-16 04:25:13 -0500

    Correct BodyController for non-movement

--- src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/WRBodyControl.java
+++ src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/WRBodyControl.java
@@ -13,6 +13,9 @@ import net.minecraft.world.entity.ai.control.BodyRotationControl;
 
 public class WRBodyControl extends BodyRotationControl
 {
+
+    private int headStableTime;
+
     public WRDragonEntity entity;
     private float lastStableYHeadRot;
 
@@ -23,18 +26,28 @@ public class WRBodyControl extends BodyRotationControl
     }
 
     public void clientTick() {
-        if (this.isMoving()) {
-            //If it's moving match the BodyRot to its movement yaw...
+        if (this.isMoving() && this.entity.getTarget() ==null) {
             this.entity.yBodyRot = this.entity.getYRot();
-            //Head will only rotate if it's offset angle to movement is too much
-            this.rotateHeadIfNecessary();
+            this.rotateHeadToMatchBodyIfNecessary();
             this.lastStableYHeadRot = this.entity.yHeadRot;
+            this.headStableTime = 0;
         } else {
-            //If it's not moving, rotate body to match head if differnt is too much
-            this.rotateBodyToMatchHeadIfNecessary();
+            //Changed from Vanilla Behavior - will attempt to update head faster
+            //This results in smoother, non-jittery body movement
+            //It will still allow a difference between head and body equal to MaxHeadYRot
+            if (Math.abs(this.entity.yHeadRot - this.lastStableYHeadRot) > 0) {
+                this.headStableTime = 0;
+                this.lastStableYHeadRot = this.entity.yHeadRot;
+                this.rotateBodyToMatchHeadIfNecessary();
+            } else {
+                ++this.headStableTime;
+                if (this.headStableTime > 10) {
+                    this.rotateBodyToFront();
+                }
+            }
         }
-        //TODO: PREVENT ROTATIONS IF ASLEEP
     }
+
 
     private boolean isMoving() {
         double d0 = this.entity.getX() - this.entity.xo;
@@ -42,11 +55,38 @@ public class WRBodyControl extends BodyRotationControl
         return d0 * d0 + d1 * d1 > (double)2.5000003E-7F;
     }
 
-    private void rotateHeadIfNecessary() {
+    private void rotateHeadToMatchBodyIfNecessary() {
         this.entity.yHeadRot = Mth.rotateIfNecessary(this.entity.yHeadRot, this.entity.yBodyRot, (float)this.entity.getMaxHeadYRot());
     }
 
     private void rotateBodyToMatchHeadIfNecessary() {
         this.entity.yBodyRot = Mth.rotateIfNecessary(this.entity.yBodyRot, this.entity.yHeadRot, (float)this.entity.getMaxHeadYRot());
+    }
+
+    private void rotateBodyToFront() {
+        int i = this.headStableTime - 10;
+        float f = Mth.clamp((float)i / 10.0F, 0.0F, 1.0F);
+        float f1 = (float)this.entity.getMaxHeadYRot() * (1.0F - f);
+        this.entity.yBodyRot = Mth.rotateIfNecessary(this.entity.yBodyRot, this.entity.yHeadRot, f1);
+    }
+
+    protected float rotlerp(float pSourceAngle, float pTargetAngle, float pMaximumChange) {
+        float f = Mth.wrapDegrees(pTargetAngle - pSourceAngle);
+        if (f > pMaximumChange) {
+            f = pMaximumChange;
+        }
+
+        if (f < -pMaximumChange) {
+            f = -pMaximumChange;
+        }
+
+        float f1 = pSourceAngle + f;
+        if (f1 < 0.0F) {
+            f1 += 360.0F;
+        } else if (f1 > 360.0F) {
+            f1 -= 360.0F;
+        }
+
+        return f1;
     }
 }
