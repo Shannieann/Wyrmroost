85deccb4f09b4d35ce2df506bda17ee6b4724743
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-23 10:42:27 -0500

    Update BFL logic for textures

--- src/main/java/com/github/shannieann/wyrmroost/client/model/entity/dragon/ModelButterflyLeviathan.java
+++ src/main/java/com/github/shannieann/wyrmroost/client/model/entity/dragon/ModelButterflyLeviathan.java
@@ -2,34 +2,41 @@ package com.github.shannieann.wyrmroost.client.model.entity.dragon;
 
 import com.github.shannieann.wyrmroost.Wyrmroost;
 import com.github.shannieann.wyrmroost.entities.dragon.EntityButterflyLeviathan;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.model.provider.data.EntityModelData;
+import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
+import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 public class ModelButterflyLeviathan extends AnimatedGeoModel<EntityButterflyLeviathan>
 {
     private static final ResourceLocation ANIMATION_RESOURCE = new ResourceLocation(Wyrmroost.MOD_ID, "animations/entity/dragon/butterfly_leviathan/butterfly_leviathan.json");
+    private static final ResourceLocation MODEL_RESOURCE = new ResourceLocation(Wyrmroost.MOD_ID,"geo/entity/dragon/butterfly_leviathan/butterfly_leviathan.geo.json");
 
-    //TODO: All model variants
     //TODO: Conduits
     @Override
     public ResourceLocation getModelLocation(EntityButterflyLeviathan object) {
-        String modelLocation = "geo/entity/dragon/butterfly_leviathan/butterfly_leviathan.geo.json";
-        return new ResourceLocation(Wyrmroost.MOD_ID, modelLocation);
+        return MODEL_RESOURCE;
     }
 
     @Override
     public ResourceLocation getTextureLocation(EntityButterflyLeviathan object) {
         int variant = object.getVariant();
-        if (variant < -1) {
-            variant = 0;
-        } else if (variant > 1) {
-            variant = 0;
+        String variantString;
+        switch (variant) {
+            case -1 -> variantString = "special";
+            case 0 -> variantString = "base0";
+            case 1 -> variantString = "base1";
+            default -> variantString = "base0";
         }
-        String textureLocation = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_" + object.getVariant() +".png";
+        String textureLocation = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_"+variantString+".png";
         return new ResourceLocation(Wyrmroost.MOD_ID, textureLocation);
     }
 
@@ -85,5 +92,39 @@ public class ModelButterflyLeviathan extends AnimatedGeoModel<EntityButterflyLev
             this.getAnimationProcessor().getBone("ineck1").setRotationX(-setExtremityPitchValue * rotationPitchMultiplier);
             this.getAnimationProcessor().getBone("itail1").setRotationX(-setExtremityPitchValue * rotationPitchMultiplier);
         }
+    }
+    public static class ButterflyLeviathanActivatedLayer<T extends EntityButterflyLeviathan> extends GeoLayerRenderer<T> {
+
+        private static final ResourceLocation BLANK_TEXTURE = new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/butterfly_leviathan/blank.png");
+
+        public ButterflyLeviathanActivatedLayer(IGeoRenderer<T> entityRendererIn) {
+            super(entityRendererIn);
+        }
+
+        @Override
+        protected ResourceLocation getEntityTexture(T entityIn) {
+            int variant = entityIn.getVariant();
+            String variantString;
+            if (entityIn.canPerformLightningAttack()) {
+                switch (variant) {
+                    case -1 -> variantString = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_special_activated.png";
+                    case 0 -> variantString = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_base0_activated.png";
+                    case 1 -> variantString = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_base1_activated.png";
+                    default -> variantString = "textures/entity/dragon/butterfly_leviathan/butterfly_leviathan_base0_activated.png";
+                }
+                return new ResourceLocation(Wyrmroost.MOD_ID,variantString);
+            }
+            return BLANK_TEXTURE;
+        }
+
+        @Override
+        public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, T entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+            RenderType cameo = getRenderType(getEntityTexture(entityLivingBaseIn));
+            matrixStackIn.pushPose();
+            this.getRenderer().render(this.getEntityModel().getModel(MODEL_RESOURCE), entityLivingBaseIn, partialTicks, cameo, matrixStackIn, bufferIn,
+                    bufferIn.getBuffer(cameo), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 1f);
+            matrixStackIn.popPose();
+        }
+
     }
 }
