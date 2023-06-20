556c3a2899430653b5c86f6dadcfeaadbe604adc
Sniffity <66455728+Sniffity@users.noreply.github.com>
2023-06-20 10:30:00 -0500

    Remove debug print statements

--- src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/goals/WRSleepGoal.java
+++ src/main/java/com/github/shannieann/wyrmroost/entities/dragon/ai/goals/WRSleepGoal.java
@@ -70,31 +70,21 @@ public class WRSleepGoal extends AnimatedGoal{
 
     @Override
     public void tick() {
-        System.out.println("LINE 1");
         LookControl lookControl = entity.getLookControl();
-        System.out.println("LINE 2");
         if (lookControl instanceof WRGroundLookControl) {
-            System.out.println("LINE 3");
             ((WRGroundLookControl) lookControl).stopLooking();
         }
-        System.out.println("LINE 4");
         if (lookControl instanceof WRSwimmingLookControl) {
-            System.out.println("LINE 5");
             ((WRSwimmingLookControl) lookControl).stopLooking();
         }
         //ToDo: Flying look Control
 
         // Heal while sleeping
-        System.out.println("LINE 6");
         if (entity.getHealth() < entity.getMaxHealth() && entity.getRandom().nextDouble() < 0.005) {
-            System.out.println("LINE 7");
             entity.heal(1);
         }
-        System.out.println("LINE 8");
         super.start(underwaterSleeping ? "sleep_water" : "sleep", 1, 20);
-        System.out.println("LINE 9");
         super.tick();
-        System.out.println("LINE 10");
     }
 
 
