5e9b10d3db9b184d0f5b8cbdfa86c6b5f9b31284
Sniffity <Sniffity@yahoo.com>
2023-12-09 18:25:04 -0500

    Dynamic textures for lightning nova

--- src/main/java/com/github/shannieann/wyrmroost/client/model/effect/ModelLightningSphere.java
+++ src/main/java/com/github/shannieann/wyrmroost/client/model/effect/ModelLightningSphere.java
@@ -8,7 +8,18 @@ import software.bernie.geckolib3.model.AnimatedGeoModel;
 public class ModelLightningSphere extends AnimatedGeoModel<EffectLightningSphere>
 {
     private static final ResourceLocation MODEL_RESOURCE = new ResourceLocation(Wyrmroost.MOD_ID, "geo/entity/effect/lightning_sphere/lightning_sphere.geo.json");
-    private static final ResourceLocation TEXTURE_RESOURCE = new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/effect/lightning_sphere/lightning_sphere.png");
+    private static final ResourceLocation[] TEXTURE_RESOURCE = new ResourceLocation[] {
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_1.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_2.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_3.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_4.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_5.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_6.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_7.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_8.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_9.png"),
+            new ResourceLocation(Wyrmroost.MOD_ID,"textures/entity/effect/lightning_nova/lightning_nova_10.png")
+    };
     private static final ResourceLocation ANIMATION_RESOURCE = new ResourceLocation(Wyrmroost.MOD_ID, "animations/entity/effect/lightning_sphere/lightning_sphere.json");
 
     @Override
@@ -18,7 +29,8 @@ public class ModelLightningSphere extends AnimatedGeoModel<EffectLightningSphere
 
     @Override
     public ResourceLocation getTextureLocation(EffectLightningSphere object) {
-        return TEXTURE_RESOURCE;
+        int index = object.tickCount % TEXTURE_RESOURCE.length;
+        return TEXTURE_RESOURCE[index];
     }
 
     @Override
