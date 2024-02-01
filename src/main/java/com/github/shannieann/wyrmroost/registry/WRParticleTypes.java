6742df3b973bc19bbe7b4d79b2bcfd8109baa448
Sniffity <Sniffity@yahoo.com>
2024-02-01 08:05:22 -0500

    Create ParticleType registry

---
+++ src/main/java/com/github/shannieann/wyrmroost/registry/WRParticleTypes.java
@@ -0,0 +1,17 @@
+package com.github.shannieann.wyrmroost.registry;
+
+import com.github.shannieann.wyrmroost.Wyrmroost;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class WRParticleTypes {
+    public static final DeferredRegister<ParticleType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Wyrmroost.MOD_ID);
+
+    public static final RegistryObject<SimpleParticleType> TEST_PARTICLE = register("test", false);
+}
