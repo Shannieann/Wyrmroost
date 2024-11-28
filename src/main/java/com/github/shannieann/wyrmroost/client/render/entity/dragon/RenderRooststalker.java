e9ded8e5e800d4e3dcc3ac02e0a94cefc05c1f74
Sniffity <Sniffity@yahoo.com>
2024-11-28 10:40:05 -0500

    Scale rooststalker, age

--- src/main/java/com/github/shannieann/wyrmroost/client/render/entity/dragon/RenderRooststalker.java
+++ src/main/java/com/github/shannieann/wyrmroost/client/render/entity/dragon/RenderRooststalker.java
@@ -3,6 +3,7 @@ package com.github.shannieann.wyrmroost.client.render.entity.dragon;
 import com.github.shannieann.wyrmroost.Wyrmroost;
 import com.github.shannieann.wyrmroost.client.model.entity.dragon.ModelRooststalker;
 import com.github.shannieann.wyrmroost.client.render.entity.dragon.layer.DragonEyesLayer;
+import com.github.shannieann.wyrmroost.entity.dragon.EntityButterflyLeviathan;
 import com.github.shannieann.wyrmroost.entity.dragon.EntityRooststalker;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -25,11 +26,14 @@ public class RenderRooststalker extends WRDragonRender<EntityRooststalker> {
 
     }
 
+
     @Override
     public void renderLate(EntityRooststalker animatable, PoseStack poseStack, float partialTick, MultiBufferSource bufferSource,
                            VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue,
                            float alpha) {
-        poseStack.scale(0.625f, 0.625f, 0.625f);
+        float ageProgressScale = animatable.getScale();
+
+        poseStack.scale(0.625f*ageProgressScale, 0.625f*ageProgressScale, 0.625f*ageProgressScale);
         super.renderLate(animatable, poseStack, partialTick, bufferSource,
                 buffer, packedLight, packedOverlay, red, green, blue,
         alpha);
