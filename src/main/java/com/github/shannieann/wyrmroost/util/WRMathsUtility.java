d0b45eea874b43ca65e65e9920da4e1784822568
Sniffity <Sniffity@yahoo.com>
2024-11-28 09:48:16 -0500

    Update WRMathsUtility.java

--- src/main/java/com/github/shannieann/wyrmroost/util/WRMathsUtility.java
+++ src/main/java/com/github/shannieann/wyrmroost/util/WRMathsUtility.java
@@ -12,8 +12,7 @@ import java.util.function.Predicate;
 
 
 public final class WRMathsUtility {
-    private WRMathsUtility()
-    {/* good try */}
+    private WRMathsUtility() {}
 
     public static final float PI = (float) Math.PI;
 
